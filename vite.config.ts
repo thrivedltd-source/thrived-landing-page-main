49f5769a1d92e888d148033d5365a5c300d8705d
Faraz Tafti <faraztafti@Farazs-Air.lan>
2026-01-29 18:47:42 +0000

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,6 @@ import { defineConfig } from "vite"
 export default defineConfig({
   base: "/thrived-landing-page-main/",
   plugins: [react()],
-  build: {
-    outDir: "docs",
-    emptyOutDir: true,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
