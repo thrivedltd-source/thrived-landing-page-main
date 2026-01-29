eecb0289cdcf2e43366d3599751cc6337055d4aa
Faraz Tafti <faraztafti@Farazs-Air.lan>
2026-01-29 18:37:14 +0000

    Build site for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: '/thrived-landing-page-main/',
+  base: "/thrived-landing-page-main/",
   plugins: [react()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 })
